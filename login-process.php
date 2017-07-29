508017f43fc18fa36013ac8dc28fa160e47f75c2
smechailes <smechailes@gmail.com>
2017-07-29 13:37:58 +0545

    cookie has been set properly in login process but its not been tested with index page yet

--- login-process.php
+++ login-process.php
@@ -2,12 +2,16 @@
 	
 $username = "admin";
 $password = "admin";
+if(isset($_POST['remember']) && $_POST['remember'] == 1){
+	setcookie('is_logged_in', 1, time()+600);
+}
 if(isset($_POST) && $_POST != ""){
 	if($username === $_POST['username']){
 		if($password === $_POST['password']){
 			$_SESSION['USERNAME'] = $_POST['username'];
 			$_SESSION['PASSWORD'] = $_POST['password'];
 			$_SESSION['SUCCESS'] = "Welcome back, ".$_SESSION['USERNAME']." !";
+
 			header('location: login.php');
  		}else{
 			$_SESSION['WARNING'] = "PASSWORD IS INCORRECT";
